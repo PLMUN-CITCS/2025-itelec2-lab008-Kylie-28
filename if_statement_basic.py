94a519a0d6d5e1a0ac224fafdfd4f65fabc71a70
kylie-28 <bolivaralexkyle_bsit@plmun.edu.ph>
2025-02-13 02:54:02 -0800

    Update if_statement_basic.py

--- if_statement_basic.py
+++ if_statement_basic.py
@@ -1,7 +1,8 @@
 # Simple If Statement Example: Checking Hot Weather
-temperature = float(input("Enter the temperature in Celsius: "))
-
+temperature = float(input("Enter the temperature in Celsius: ")
+                    
 if temperature > 30:
+    
     print("It's a hot day! Stay hydrated and cool. ")
 
 print("Have a great day!")
