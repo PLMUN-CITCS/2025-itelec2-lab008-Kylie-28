c66938204f251d454fe7ab359314bd3bd630daa7
kylie-28 <bolivaralexkyle_bsit@plmun.edu.ph>
2025-02-13 02:52:08 -0800

    Update if_statement_basic.py

--- if_statement_basic.py
+++ if_statement_basic.py
@@ -3,6 +3,6 @@
 temperature = float(input("Enter the temperature in Celsius: "))
 
 if temperature > 30:
-    print("It's a hot day! Stay hydrated and cool. 🥵💧")
+    print("It's a hot day! Stay hydrated and cool. ")
 
 print("Have a great day!")
